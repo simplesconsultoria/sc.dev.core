6e9904f578d5808c85fc7bd8b43cbc093e4a62cf
erico_andrei <erico_andrei@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-07-07 05:05:31 +0000

    Add sphinx doc upload entry point

---
+++ sc/dev/core/sphinx_pypi.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+import ConfigParser
+import glob
+import os
+import zest.releaser.utils
+
+SECTION = 'build_sphinx'
+
+def upload(context):
+    ''' Upload sphinx doc'''
+    config = ConfigParser.ConfigParser()
+    config.read(os.path.join(context['working_dir'], 'setup.cfg'))
+    if SECTION not in config.sections():
+        return None
+    
+    build_command = 'build_sphinx'
+    upload_command = 'upload_sphinx'
+    if not zest.releaser.utils.ask('Upload documentation to http://packages.python.org'):
+        return
+    # build documentation
+    shell_command = utils.setup_py(build_command)
+    result = utils.system(shell_command)
+    
+    # upload documentation
+    shell_command = utils.setup_py(upload_command)
+    result = utils.system(shell_command)
+    
